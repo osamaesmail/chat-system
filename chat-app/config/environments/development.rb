58ae90425604913c81f22f0a9c2915f0f26b64b0
Osama Esmail <osamaesmail2@gmail.com>
2019-09-18 02:46:40 +0200

    use redis

--- chat-app/config/environments/development.rb
+++ chat-app/config/environments/development.rb
@@ -14,18 +14,17 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    config.cache_store = :redis_store, {
+      host: "localhostt",
+      port: 6379,
+      db: 0,
+      password: "mysecret",
+      namespace: "cache"
+    }, {
+      expires_in: 60.minutes
     }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
